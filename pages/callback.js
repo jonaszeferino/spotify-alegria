2a667bb92a48e4d67b032d186fdd14a61fc3af91
jonaszeferino <jonaszeferino@gmail.com>
2023-12-23 22:42:52 -0300

    new index page, without automatic authenticantion reader

--- pages/callback.js
+++ pages/callback.js
@@ -1,25 +1,47 @@
-import { useEffect } from "react";
+// pages/callback.js
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 const CallbackPage = () => {
   const router = useRouter();
+  const [loading, setLoading] = useState(true);
+  const [accessToken, setAccessToken] = useState(null);
 
   useEffect(() => {
     const { code } = router.query;
+    const exchangeCodeForToken = async () => {
+      try {
+        console.log("Código recebido:", code);
+
+        const response = await fetch(`/api/exchange-code?code=${code}`);
+        const data = await response.json();
+        console.log("Token de acesso recebido:", data.accessToken);
+        setAccessToken(data.accessToken);
+        router.push("/");
+      } catch (error) {
+        console.error("Erro ao trocar código por token:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
 
     if (code) {
-      fetch(`/api/exchange-code?code=${code}`)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("Token de acesso:", data.accessToken);
-        })
-        .catch((error) => {
-          console.error("Erro ao trocar código por token:", error);
-        });
+      exchangeCodeForToken();
     }
   }, [router.query]);
 
-  return <div>Redirecionando...</div>;
+  return (
+    <div>
+      {loading ? (
+        <p>Redirecionando...</p>
+      ) : (
+        <p>
+          O processo de autenticação foi concluído. Você pode fechar esta
+          janela.
+        </p>
+      )}
+    </div>
+  );
 };
 
 export default CallbackPage;
